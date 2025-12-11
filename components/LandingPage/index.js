78c40cec4d3791d75f74943af424f589fc40b26e
muhdzaheermv <muhdzaheermv@gmail.com>
2025-12-11 17:39:36 +0530

    banner images

--- components/LandingPage/index.js
+++ components/LandingPage/index.js
@@ -4,17 +4,17 @@ import React, { useEffect, useLayoutEffect, useRef, useState } from 'react'
 import { gsap, Expo, Power2, Power4, Power1, Power3, Power0, Linear } from "gsap";
 import { CustomEase } from "gsap/dist/CustomEase";
 import Hero from './hero';
-import dancers from "@/public/images/dancers.jpeg"
+import dancers from "@/public/images/dancers.png"
 import elephant from "@/public/images/elephant.jpeg"
 import femaledancer from "@/public/images/femaledancer.jpeg"
 import ghoomar from "@/public/images/ghoomar.jpeg"
 import knight from "@/public/images/knight.jpeg"
-import main from "@/public/images/main.jpeg"
-import mountainman from "@/public/images/mountainman.jpeg"
-import theyyam_1 from "@/public/images/theyyam 1.jpeg"
-import theyyam_2 from "@/public/images/theyyam 2.png"
-import theyyam_3 from "@/public/images//theyyam 3.jpeg"
-import theyyam_4 from "@/public/images/theyyam 4.png"
+// import main from "@/public/images/main.jpeg"
+// import mountainman from "@/public/images/mountainman.jpeg"
+// import theyyam_1 from "@/public/images/theyyam 1.jpeg"
+// import theyyam_2 from "@/public/images/theyyam 2.png"
+// import theyyam_3 from "@/public/images//theyyam 3.jpeg"
+import theyyam_4 from "@/public/images/theyyam 4.jpg"
 
 function convertRemToPixels(rem) {
     return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
@@ -32,18 +32,18 @@ const getImages = () => {
 
     let imagefiles = [
         // { file: '/ashoka.png', prompt: 'Emperor Ashoka with a cape looks over a kingdom' },
-        { file: dancers, prompt: 'Traditional Kerala dancers perform around a lake' },
-        { file: elephant, prompt: 'An elephant with ornaments during a festival in Kerala' },
-        { file: femaledancer, prompt: 'A female dancer performs in the courtyard of an Indian palace' },
-        { file: ghoomar, prompt: 'Two women performing ghoomar dance in a dimly lit stage' },
-        { file: knight, prompt: 'An Indian emperor in a forest at night with his horse' },
-        { file: main, prompt: 'The face of an imaginary Indian folk art performer.' },
-        { file: mountainman, prompt: 'An old emperor looks over his kingdom from a mountain' },
+        { file: dancers, prompt: 'Aspire College of Advanced Studies' },
+        { file: elephant, prompt: 'Aspire College of Advanced Studies' },
+        { file: femaledancer, prompt: 'Aspire College of Advanced Studies  ' },
+        { file: ghoomar, prompt: 'Aspire College of Advanced Studies ' },
+        { file: knight, prompt: 'Aspire College of Advanced Studies' },
+        // { file: main, prompt: 'Aspire College of Advanced Studies ' },
+        // { file: mountainman, prompt: ' Aspire College of Advanced Studies' },
         // { file: '/mountaindance.png', prompt: 'three indian traditional women dancing in a field' },
-        { file: theyyam_1, prompt: 'Theyyam performing in the dark on a field' },
-        { file: theyyam_2, prompt: 'Portrait of a theyyam against a dark background' },
-        { file: theyyam_3, prompt: 'Theyyam performing in the middle of a forest.' },
-        { file: theyyam_4, prompt: 'A theyyam artist against a dark background' },
+        // { file: theyyam_1, prompt: 'Aspire College of Advanced Studies ' },
+        // { file: theyyam_2, prompt: 'Aspire College of Advanced Studies ' },
+        // { file: theyyam_3, prompt: 'Aspire College of Advanced Studies' },
+        { file: theyyam_4, prompt: 'Aspire College of Advanced Studies ' },
     ]
     imagefiles = imagefiles
         .map((value,idx) => ({ value, sort: random(time + idx) }))

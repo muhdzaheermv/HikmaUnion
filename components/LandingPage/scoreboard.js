22341f7bb343b05707bd84c8b2155d584efca35d
muhdzaheermv <muhdzaheermv@gmail.com>
2026-01-20 15:08:06 +0530

    score update

--- components/LandingPage/scoreboard.js
+++ components/LandingPage/scoreboard.js
@@ -38,11 +38,11 @@ export default function Rajan() {
   { name: "200 M Race (Men)", scores: [0, 3, 0, 5] },
   { name: "200 M Race (Women)", scores: [3, 0, 0, 6] },
 
-  { name: "800 M Race (Men)", scores: [0, 1, 8, 0] },
+  { name: "800 M Race (Men)", scores: [1, 0, 8, 0] },
   { name: "800 M Race (Women)", scores: [0, 5, 3, 1] },
 
-  { name: "1500 M Race (Men)", scores: [5, 3, 0, 0] },
-  { name: "1500 M Race (Women)", scores: [0, 0, 0, 9] },
+  { name: "1500 M Race (Men)", scores: [5, 3, 0, 9] },
+  { name: "1500 M Race (Women)", scores: [5, 3, 0, 9] },
 
   { name: "4×100 M Relay (Men)", scores: [3, 0, 5, 10] },
   { name: "4×100 M Relay (Women)", scores: [0, 3, 5, 10] }
